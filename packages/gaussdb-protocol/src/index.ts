d96943ab65b3cf4a88225c3c9f91f6c9ff5d97d2
happygame <happygame1024@gmail.com>
2026-01-22 20:03:54 +0800

    feat(protocol): extend BufferReader with configurable endianness
    - Add endian parameter ('be' | 'le'), default 'be' for backward compatibility
    - Add uint16(), peekByte(), uint64Parts(), remaining() methods

--- packages/gaussdb-protocol/src/index.ts
+++ packages/gaussdb-protocol/src/index.ts
@@ -1,6 +1,7 @@
 import { DatabaseError } from './messages'
 import { serialize } from './serializer'
 import { Parser, MessageCallback } from './parser'
+import { BufferReader, Endian } from './buffer-reader'
 
 export function parse(stream: NodeJS.ReadableStream, callback: MessageCallback): Promise<void> {
   const parser = new Parser()
@@ -8,4 +9,5 @@ export function parse(stream: NodeJS.ReadableStream, callback: MessageCallback):
   return new Promise((resolve) => stream.on('end', () => resolve()))
 }
 
-export { serialize, DatabaseError }
+export { serialize, DatabaseError, BufferReader }
+export type { Endian }
